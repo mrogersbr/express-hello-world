4f6a7be7e7a15b15ed6e3267b4ce5a963e20e628
mrogersbr <76775842+mrogersbr@users.noreply.github.com>
2024-10-01 12:40:04 -0300

    Create BibbidiBobbidiBoutique.js

---
+++ BibbidiBobbidiBoutique.js
@@ -0,0 +1,9 @@
+class BibbidiBobbidiBoutique extends BookableExperience {
+
+   constructor() {
+      super('Bibbidi Bobbidi Boutique',
+         'https://disneyworld.disney.go.com/enchanting-extras-collection/booking-bibbidi-bobbidi-boutique-magic-kingdom/',
+         'https://disneyworld.disney.go.com/sa-api/api/v1/experience/offers/290140?entityType=activity-product&startDate=${data}&endDate=${data}&affiliations=DISNEY_STREAMING&child=1'
+      );
+   }
+}
