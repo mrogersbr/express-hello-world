65aa7ed1ddb57d3b13811d53a0ed5ed9c70a92a5
mrogersbr <76775842+mrogersbr@users.noreply.github.com>
2024-10-01 12:46:48 -0300

    Update BookableExperienceRequester.js

--- src/BookableExperienceRequester.js
+++ src/BookableExperienceRequester.js
@@ -1,1 +1,33 @@
+class BookableExperienceRequester {
 
+   #bookableExperience; //[BookableExperience, BookableExperience, ]
+
+   constructor(bookableExperience) {
+      this.#bookableExperience = bookableExperience;
+   }
+
+   search(dateArray) {
+
+      let httpResponses = [];
+
+      dateArray.forEach((date, i) => {
+         this.#bookableExperience.forEach((bookableExperience, i) => {
+
+            let xhr = new XMLHttpRequest();
+            let url = bookableExperience.getInterpolateURL(['${data}'], [date]);
+            xhr.open("GET", url, false);
+
+            try {
+               xhr.send(null);
+            } catch(e) {
+               window.open(bookableExperience.url,'_self');
+            }
+
+            httpResponses.push([bookableExperience.description, date, xhr.responseText);
+
+         });
+      });
+
+      return httpResponses; //[[bookableExperience.description(), date, xhr.responseText], [bookableExperience.description(), date, xhr.responseText], ...]
+   }
+}
