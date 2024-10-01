f44a40ce882fc892f48773782c729fbe6a3c6e38
mrogersbr <76775842+mrogersbr@users.noreply.github.com>
2024-10-01 12:39:19 -0300

    Create BookableExperience.js

---
+++ BookableExperience.js
@@ -0,0 +1,32 @@
+class BookableExperience {
+
+   #description;
+   #url;
+   #urlTemplateString;
+
+   constructor(description, url, urlTemplateString) {
+
+      this.#description = description;
+      this.#url = url;
+      this.#urlTemplateString = urlTemplateString;
+
+   }
+
+   get description() {
+      return this.#description;
+   }
+
+   get url() {
+      return this.#url;
+   }
+
+   getInterpolateURL(params, values) { //[params] [values] -> params.length == values.length
+
+      params.forEach((param, i) => {
+         this.#urlTemplateString = this.#urlTemplateString.replaceAll(param, values[i]);
+      });
+
+      return this.#urlTemplateString;
+   }
+
+}
